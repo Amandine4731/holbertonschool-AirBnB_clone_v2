7d7fb4576dd0e6be2a4dd3bb9b3a230a10ef0297
Amandine4731 <5618@holbertonstudents.com>
2023-04-04 13:59:35 -0700

    with 2 route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,13 +8,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hbnb')
+@app.route('/')
 def hello():
     strict_slashes = False
     return 'Hello HBNB!'
 
+@app.route('/hbnb')
 def hbnb():
+    strict_slashes = False
     return 'HBNB'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
