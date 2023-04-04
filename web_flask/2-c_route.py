7cb9a8fe44059e81d412ffaf02ba812af846159a
Amandine4731 <5618@holbertonstudents.com>
2023-04-04 14:38:47 -0700

    replace underscore with space

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    program to start a flask web application
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    strict_slashes = False
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    strict_slashes = False
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    strict_slashes = False
+    return f'C {escape(text)}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
