8a6dadccff3d4cb68b99cdb89ac4fbc20d320915
Amandine4731 <5618@holbertonstudents.com>
2023-04-05 07:10:58 -0700

    documentation

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def n_num(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def n_num_html(n):
+def number(n):
     return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
