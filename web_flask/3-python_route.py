f89ec2f21410b8f20d71a575fdfa001aa788c4f5
Amandine4731 <5618@holbertonstudents.com>
2023-04-05 01:20:57 -0700

    documentation

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -29,7 +29,7 @@ def c_text(text):
 
 
 @app.route('/python/<text>')
-def c_text(text = "is cool"):
+def python_text(text = "is cool"):
     strict_slashes = False
     var = f'Python {text}'
     return var.replace("_", " ")
