e29aa36ef25bf71bda0e83c523249876f296fa0b
Amandine4731 <5618@holbertonstudents.com>
2023-04-05 07:02:53 -0700

    documentation

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -9,43 +9,37 @@ from markupsafe import escape
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes = False)
 def hello():
-    strict_slashes = False
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes = False)
 def hbnb():
-    strict_slashes = False
     return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes = False)
 def c_text(text):
-    strict_slashes = False
     var = f'C {text}'
     return var.replace("_", " ")
 
 
-@app.route("/python/", defaults={'text': 'is cool'})
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes = False)
 @app.route("/python/<text>")
 def python_text(text):
-    strict_slashes = False
     var = f'Python {text}'
     return var.replace("_", " ")
 
 
-@app.route('/number/<int:n>')
+@app.route('/number/<int:n>'; strict_slashes = False)
 def n_num(n):
-    strict_slashes = False
     var = f'{n} is a number'
     return var
 
 
-@app.route('/number_template/<int:n>')
-def n_num(n):
-    strict_slashes = False
+@app.route('/number_template/<int:n>', strict_slashes = False)
+def n_num_html(n):
     return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
